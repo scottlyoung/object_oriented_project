cffbcf897a22c55ed9fa391ebc869a0a2c51e191
amjaad <amjad.alharbi@colorado.edu>
2018-04-11 19:42:03 -0600

    basic search view-controller

---
+++ client/client/JListCustomRenderer.java
@@ -0,0 +1,52 @@
+package client;
+
+import server.item;
+
+import java.awt.BorderLayout;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+    public class JListCustomRenderer extends JFrame {
+
+        private int width = 350;
+        private int height = 200;
+        private JList<Item> listItem;
+
+
+        public JListCustomRenderer() {
+            // add main panel
+            add(createMainPanel());
+            // set display
+            setTitle("Search Results");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(width, height);
+            setLocationRelativeTo(null);
+            setVisible(true);
+        }
+
+        private JPanel createMainPanel() {
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+            // create list item and set to scrollpane and add to panel
+            panel.add(new JScrollPane(listItem = createListItem()),
+                    BorderLayout.CENTER);
+            return panel;
+        }
+
+        private JList<Item> createListItem() {
+            // create List model
+            DefaultListModel<Item> model = new DefaultListModel<>();
+            // add item to model
+            model.addElement(new Song("Renegades", "X Ambassadors", "Indie", "VHS", 195));
+            model.addElement(new Song("Say you won't let go", "James Arthur", "Pop", "Back from the Edge", 195));
+
+            // create JList with model
+            JList<Item> list = new JList<Item>(model);
+            // set cell renderer
+            list.setCellRenderer(new ItemRenderer());
+            list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            return list;
+        }
+
+    }
+
--- client/client/Main.java
+++ client/client/Main.java
@@ -2,7 +2,8 @@ package client;
 
 public class Main {
     public static void main(String[] args) {
-        App app = new App();
+        App app = new client.App();
+
         app.setVisible(true);
     }
 }
